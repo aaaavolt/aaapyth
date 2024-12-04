b8ebc792bfb36a3b6b7b1fd9ba180c44d8d39342
anton maltshev <volttazer@gmail.com>
2024-12-04 19:09:30 +0300

    aa1py

--- json/mainjson.py
+++ json/mainjson.py
@@ -1,6 +1,13 @@
 import json
-josn_sreing ='{"name": "alise","age":"25"}'
-data = json.loads(josn_sreing)
-print(data["name"]) 
-print(data["age"]) 
+# josn_sreing ='{"name": "alise","age":"25"}'
+# data = json.loads(josn_sreing)
+# print(data["name"]) 
+# print(data["age"]) 
  
+python_data = {
+    "name": "leha",
+    "gruz": 200,
+    "skills": []
+}
+json_string = json.dumps(python_data, indent =4)
+print (json_string)
